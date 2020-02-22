e8c7a48be185f64f488833ec264bec6a4bd05f21
ori <oriashur155@gmail.com>
2020-02-22 15:43:15 +0200

    basic comparision functionality works

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,4 @@
-
-
+use core::cmp;
 
 struct CmpByKey<'k, T, K> {
     inner: T,
@@ -14,6 +13,23 @@ impl<'k, T,K> CmpByKey<'k, T,K> {
     }
 }
 
+impl<T, K: PartialOrd> PartialOrd for CmpByKey<'_, T, K> {
+    fn partial_cmp(&self, other: &Self) -> Option<cmp::Ordering> {
+        let self_key:  K = (self.key_func)(&self.inner);
+        let other_key: K = (other.key_func)(&other.inner);
+        self_key.partial_cmp(&other_key)
+    }
+}
+
+impl<T, K: PartialEq> PartialEq for CmpByKey<'_, T, K> {
+    fn eq(&self, other: &Self) -> bool {
+        let self_key:  K = (self.key_func)(&self.inner);
+        let other_key: K = (other.key_func)(&other.inner);
+        self_key == other_key
+    }
+}
+
+impl<T, K: Eq> Eq for CmpByKey<'_, T, K> {}
 
 #[cfg(test)]
 mod tests {
@@ -22,5 +38,14 @@ mod tests {
     #[test]
     fn can_wrap_i32_with_closure() {
         let x = CmpByKey::new(32, &|x: &i32| -> i32 { -*x });
+    }
+
+    #[test]
+    fn can_compare_two_i32_with_reversed_order() {
+        let x32 = CmpByKey::new(32, &|x: &i32| -> i32 { -*x });
+        let y33 = CmpByKey::new(33, &|x: &i32| -> i32 { -*x });
+
+        assert!(x32 > y33, "should be in reversed order" );
+
     }
 }
