72ec1b5c712453f1bcb6050621796c0f55ca4535
ori <oriashur155@gmail.com>
2020-02-22 15:30:11 +0200

    basic wrap can be created

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,26 @@
+
+
+
+struct CmpByKey<'k, T, K> {
+    inner: T,
+    key_func: &'k dyn Fn(&T) -> K
+}
+
+impl<'k, T,K> CmpByKey<'k, T,K> {
+    pub fn new<'kf>(value: T, key_func: &'kf dyn Fn(&T) -> K) -> Self
+        where 'kf: 'k
+    {
+        Self{ inner: value, key_func }
+    }
+}
+
+
 #[cfg(test)]
 mod tests {
+    use crate::CmpByKey;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn can_wrap_i32_with_closure() {
+        let x = CmpByKey::new(32, &|x: &i32| -> i32 { -*x });
     }
 }
